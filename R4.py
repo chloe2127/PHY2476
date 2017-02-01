0859123fdea01d0f32987d45d5fa636b6915b727
chloe2127 <chloe.lefebvre@videotron.ca>
2017-01-31 19:26:34 -0500

    Add files via upload

---
+++ R4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+###########################################################
+# PHY2476 - 4 Densité électronique d'un plasma
+# Chloé Lefebvre, Olivier Petit Vincent
+# 31 janvier et 7 février 2017
+###########################################################
+
+###########################################################
+# Importation des modules et des fonctions externes
+import numpy as np
+import matplotlib.pyplot as plt
+
+###########################################################
+# Définition des constantes
+
+# lecture des données txt
+tab1s = np.loadtxt('1s.txt')

602038295aecd601f6f7a8e30dafce829140ecf9
chloe2127 <chloe.lefebvre@videotron.ca>
2017-02-24 13:16:12 -0500

    Add files via upload

--- R9.py
+++ R9.py
@@ -15,13 +15,14 @@ import matplotlib.pyplot as plt
 # Définition de fonctions locales
 
 def comparaison(noir, tab, Pmax, dim, Id):			#paramètres à comparer pour grosse vs petite
-	Is = noir[:,0]/(np.exp((q*noir[:,1])/k*T)-1)	#courant de saturation qu'on calcule dans le noir (pour I=Id)
+	Is = noir[:,0]/(np.exp((q*noir[:,1])/(k*T))-1)	#courant de saturation qu'on calcule dans le noir (pour I=Id)
 	phi = Pmax/(dim[0,0]*dim[1,0])					#phi = Pmax/surface
-	plt.scatter(tab[:,1], np.log(Id[:]), marker='.', color='k')
+	plt.scatter(tab[:,1], np.log(Id[:]), marker='.', color='k') #méthode selon Simon
 	plt.show()										#juste pour vérifier que le choix des pts pour la pente et correct
 	N = (tab[-1,1]-tab[-10,1])/(np.log(Id[-1])-np.log(Id[-10])) #inverse pente ln(Id) vs V
+	Ntheo = (q*tab[450:500,1])/(k*T*(np.log(Id[450:500]/Is[450:500])-1)) 	#isoler n selon la formule donnée (on choisit juste 50 valeurs pour avoir les memes longueurs de arrays)
 
-	return Is, phi, N
+	return Is, phi, N,  Ntheo 						#faire des moyennes pour n?
 
 def magie(tab):
 	
@@ -109,7 +110,7 @@ tabg2 = np.loadtxt('g_2.csv', delimiter=';', usecols=range(2))
 tabg3 = np.loadtxt('g_3.csv', delimiter=';', usecols=range(2))
 tabg4 = np.loadtxt('g_4.csv', delimiter=';', usecols=range(2))
 
-plt.scatter(tabg1[50:,1], tabg1[50:,0], marker = '.')
+plt.scatter(tabg1[50:,1], tabg1[50:,0], marker = '.') 		#exemple de courbe I-V
 plt.axhline(0, color='k')
 plt.axvline(0, color='k')
 plt.xlabel('Tension (V)')
@@ -123,7 +124,6 @@ plt.show()
 
 #ps1:
 valsps1_1, Idps1_1 = magie(tabps1_1)
-print(valsps1_1)
 valsps1_2, Idps1_2 = magie(tabps1_2)
 valsps1_3, Idps1_3 = magie(tabps1_3)
 valsps1_4, Idps1_4 = magie(tabps1_4)
@@ -165,19 +165,21 @@ valspp4_3, Idpp4_3 = magie(tabpp4_3)
 valspp4_4, Idpp4_4 = magie(tabpp4_4)
 
 #noir:
-isp, phip, Np = comparaison(pnoir, tabps1_1, valsps1_1[4], dimp, Idps1_1)
+isp, phip, Np, Ntheop = comparaison(pnoir, tabps1_1, valsps1_1[4], dimp, Idps1_1)
+print(isp, phip, Np, Ntheop)
 
 #graphiques pour 1 diode pour 4 ampoule (effet de l'intensité de l'illumination)
 
 #Icc et Pmax
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-ax1.scatter(np.array([11, 15, 25, 100]), np.array([valsps1_1[0], valsps1_2[0], valsps1_3[0], valsps1_4[0]]), label = 'Icc')
-ax2.scatter(np.array([11, 15, 25, 100]), np.array([valsps1_1[4], valsps1_2[4], valsps1_3[4], valsps1_4[4]]), label = 'Pmax')
+ax1.scatter(np.array([11, 15, 25, 100]), np.array([valsps1_1[0], valsps1_2[0], valsps1_3[0], valsps1_4[0]]), color = 'c', label = 'Icc')
+ax2.scatter(np.array([11, 15, 25, 100]), np.array([valsps1_1[4], valsps1_2[4], valsps1_3[4], valsps1_4[4]]), color = 'm', label = 'Pmax')
 ax1.set_xlabel("Intensité d'illumination (W)")
 ax1.set_ylabel('Courant (A)')
 ax2.set_ylabel('Puissance (W)')
-plt.legend(loc='best')
+ax1.legend(loc='upper left')
+ax2.legend(loc='lower left')
 plt.show()
 
 #Vco
@@ -187,6 +189,48 @@ plt.ylabel('Voltage (V)')
 plt.legend(loc='best')
 plt.show()
 
+#Icc, Vco, Ipm, Vpm, Pmax								
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.scatter(0, valsps1_1[0], color = 'c')
+ax1.text(0, valsps1_1[0], r'$I_{cc}$')
+ax1.scatter(valsps1_1[1], 0, color = 'c')
+ax1.text(valsps1_1[1], 0, r'$V_{co}$')
+ax2.scatter(valsps1_1[6], valsps1_1[4], color = 'c')
+ax2.text(valsps1_1[6], valsps1_1[4], r'$P_{max}$')
+ax1.scatter(valsps1_1[6], valsps1_1[5], color = 'c', label = '100W')
+ax1.text(valsps1_1[6], valsps1_1[5], r'($V{pm}, I_{pm}$)')
+ax1.scatter(0, valsps1_2[0], color = 'm')
+ax1.text(0, valsps1_2[0], r'$I_{cc}$')
+ax1.scatter(valsps1_2[1], 0, color = 'm')
+ax1.text(valsps1_2[1], 0, r'$V_{co}$')
+ax2.scatter(valsps1_2[6], valsps1_2[4], color = 'm')
+ax2.text(valsps1_2[6], valsps1_2[4], r'$P_{max}$')
+ax1.scatter(valsps1_2[6], valsps1_2[5], color = 'm', label = '25W')
+ax1.text(valsps1_2[6], valsps1_2[5], r'($V{pm}, I_{pm}$)')
+ax1.scatter(0, valsps1_3[0], color = 'b')
+ax1.text(0, valsps1_3[0], r'$I_{cc}$')
+ax1.scatter(valsps1_3[1], 0, color = 'b')
+ax1.text(valsps1_3[1], 0, r'$V_{co}$')
+ax2.scatter(valsps1_3[6], valsps1_3[4], color = 'b')
+ax2.text(valsps1_3[6], valsps1_3[4], r'$P_{max}$')
+ax1.scatter(valsps1_3[6], valsps1_3[5], color = 'b', label = '15W')
+ax1.text(valsps1_3[6], valsps1_3[5], r'($V{pm}, I_{pm}$)')
+ax1.scatter(0, valsps1_4[0], color = 'g')
+ax1.text(0, valsps1_4[0], r'$I_{cc}$')
+ax1.scatter(valsps1_4[1], 0, color = 'g')
+ax1.text(valsps1_4[1], 0, r'$V_{co}$')
+ax2.scatter(valsps1_4[6], valsps1_4[4], color = 'g')
+ax2.text(valsps1_4[6], valsps1_4[4], r'$P_{max}$')
+ax1.scatter(valsps1_4[6], valsps1_4[5], color = 'g', label = '11W')
+ax1.text(valsps1_4[6], valsps1_4[5], r'($V{pm}, I_{pm}$)')
+ax1.set_xlabel('Tension (V)')
+ax1.set_ylabel('Courant (A)')
+ax2.set_ylabel('Puissance (W)')
+ax1.legend(loc='upper left')
+ax2.legend(loc='lower left')
+plt.show()
+
 #+ faire un tableau pour Rs et Rp (pour le rapport)
 
 #graphiques pour 1-2-3-4 diodes pour 1 ampoule (1) en serie et en parallele
@@ -194,16 +238,73 @@ plt.show()
 #Icc et Pmax
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-ax1.scatter(np.array([1, 2, 3, 4]), np.array([valsps1_1[0], valsps2_1[0], valsps3_1[0], valsps4_1[0]]), label = 'Icc en série')
-ax1.scatter(np.array([1, 2, 3, 4]), np.array([valsps1_1[0], valspp2_1[0], valspp3_1[0], valspp4_1[0]]), label = 'Icc en parallèle')
-ax2.scatter(np.array([1, 2, 3, 4]), np.array([valsps1_1[4], valsps2_1[4], valsps3_1[4], valsps4_1[4]]), label = 'Pmax en série')
-ax2.scatter(np.array([1, 2, 3, 4]), np.array([valsps1_1[4], valspp2_1[4], valspp3_1[4], valspp4_1[4]]), label = 'Pmax en parllèle')
+ax1.scatter(np.array([1, 2, 3, 4]), np.array([valsps1_1[0], valsps2_1[0], valsps3_1[0], valsps4_1[0]]), color = 'c', label = 'Icc en série')
+ax1.scatter(np.array([1, 2, 3, 4]), np.array([valsps1_1[0], valspp2_1[0], valspp3_1[0], valspp4_1[0]]), color = 'm', label = 'Icc en parallèle')
+ax2.scatter(np.array([1, 2, 3, 4]), np.array([valsps1_1[4], valsps2_1[4], valsps3_1[4], valsps4_1[4]]), color = 'r', label = 'Pmax en série')
+ax2.scatter(np.array([1, 2, 3, 4]), np.array([valsps1_1[4], valspp2_1[4], valspp3_1[4], valspp4_1[4]]), color = 'b', label = 'Pmax en parllèle')
 ax1.set_xlabel("Intensité d'illumination (W)")
 ax1.set_ylabel('Courant (A)')
 ax2.set_ylabel('Puissance (W)')
-plt.legend(loc='best')
+ax1.legend(loc='upper left')
+ax2.legend(loc='lower left')
 plt.show()
 
+#Icc, Vco, Ipm, Vpm, Pmax
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.scatter(0, valsps2_1[0], color = 'c')
+ax1.text(0, valsps2_1[0], r'$I_{cc}$')
+ax1.scatter(valsps2_1[1], 0, color = 'c')
+ax1.text(valsps2_1[1], 0, r'$V_{co}$')
+ax2.scatter(valsps2_1[6], valsps2_1[4], color = 'c')
+ax2.text(valsps2_1[6], valsps2_1[4], r'$P_{max}$')
+ax1.scatter(valsps2_1[6], valsps2_1[5], color = 'c', label = '2 en série')
+ax1.text(valsps2_1[6], valsps2_1[5], r'($V{pm}, I_{pm}$)')
+ax1.scatter(0, valspp2_1[0], color = 'b')
+ax1.text(0, valspp2_1[0], r'$I_{cc}$')
+ax1.scatter(valspp2_1[1], 0, color = 'b')
+ax1.text(valspp2_1[1], 0, r'$V_{co}$')
+ax2.scatter(valspp2_1[6], valspp2_1[4], color = 'b')
+ax2.text(valspp2_1[6], valspp2_1[4], r'$P_{max}$')
+ax1.scatter(valspp2_1[6], valspp2_1[5], color = 'b', label = '2 en parallèle')
+ax1.text(valspp2_1[6], valspp2_1[5], r'($V{pm}, I_{pm}$)')
+ax1.scatter(0, valsps3_1[0], color = 'm')
+ax1.text(0, valsps3_1[0], r'$I_{cc}$')
+ax1.scatter(valsps3_1[1], 0, color = 'm')
+ax1.text(valsps3_1[1], 0, r'$V_{co}$')
+ax2.scatter(valsps3_1[6], valsps3_1[4], color = 'm')
+ax2.text(valsps3_1[6], valsps3_1[4], r'$P_{max}$')
+ax1.scatter(valsps3_1[6], valsps3_1[5], color = 'm', label = '3 en série')
+ax1.text(valsps3_1[6], valsps3_1[5], r'($V{pm}, I_{pm}$)')
+ax1.scatter(0, valspp3_1[0], color = 'r')
+ax1.text(0, valspp3_1[0], r'$I_{cc}$')
+ax1.scatter(valspp3_1[1], 0, color = 'r')
+ax1.text(valspp3_1[1], 0, r'$V_{co}$')
+ax2.scatter(valspp3_1[6], valspp3_1[4], color = 'r')
+ax2.text(valspp3_1[6], valspp3_1[4], r'$P_{max}$')
+ax1.scatter(valspp3_1[6], valspp3_1[5], color = 'r', label = '3 en parallèle')
+ax1.text(valspp3_1[6], valspp3_1[5], r'($V{pm}, I_{pm}$)')
+ax1.scatter(0, valsps4_1[0], color = 'g')
+ax1.text(0, valsps4_1[0], r'$I_{cc}$')
+ax1.scatter(valsps4_1[1], 0, color = 'g')
+ax1.text(valsps4_1[1], 0, r'$V_{co}$')
+ax2.scatter(valsps4_1[6], valsps4_1[4], color = 'g')
+ax2.text(valsps4_1[6], valsps4_1[4], r'$P_{max}$')
+ax1.scatter(valsps4_1[6], valsps4_1[5], color = 'g', label = '4 en série')
+ax1.text(valsps4_1[6], valsps4_1[5], r'($V{pm}, I_{pm}$)')
+ax1.scatter(0, valspp4_1[0], color = 'y')
+ax1.text(0, valspp4_1[0], r'$I_{cc}$')
+ax1.scatter(valspp4_1[1], 0, color = 'y')
+ax1.text(valspp4_1[1], 0, r'$V_{co}$')
+ax2.scatter(valspp4_1[6], valspp4_1[4], color = 'y')
+ax2.text(valspp4_1[6], valspp4_1[4], r'$P_{max}$')
+ax1.scatter(valspp4_1[6], valspp4_1[5], color = 'y', label = '4 en parallèle')
+ax1.text(valspp4_1[6], valspp4_1[5], r'($V{pm}, I_{pm}$)')
+ax1.set_xlabel('Tension (V)')
+ax1.set_ylabel('Courant (A)')
+ax2.set_ylabel('Puissance (W)')
+ax1.legend(loc='upper left')
+plt.show()
 
 #GROSSE
 
@@ -220,7 +321,8 @@ valsg3, Idg3 = magie(tabg3)
 valsg4, Idg4 = magie(tabg4)
 
 #noir:
-isg, phig, Ng = comparaison(gnoir, tabg1, valsg1[4], dimp, Idg1)
+isg, phig, Ng, Ntheog = comparaison(gnoir, tabg1, valsg1[4], dimp, Idg1)
+print(isg, phig, Ng, Ntheog)
 
 
 
